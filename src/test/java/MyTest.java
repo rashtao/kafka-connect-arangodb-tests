dcf06cd2b6e1ededd7f730b2addee9edebe87007
Michele Rastelli <michele@arangodb.com>
2023-07-04 11:04:52 +0200

    mvn setup

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyTest {
+
+    @Test
+    void hello() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
